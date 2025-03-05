8dc88f5230dc53c44f3616d1b3a195779b15c423
matin <matinehelahi@gmail.com>
2025-03-05 10:54:06 +0330

    change weather project

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,32 @@ const searchBtn = document.querySelector('.search__btn');
 const apikey = '7843e467f3e199e4211f91e8222591eb';
 
 searchBtn.addEventListener('click', () => {
-    if (cityInput.value.trim() != '')
-        updateWetherInfo();
-    cityInput.value = '';
-    cityInput.blur();
-});
-
-cityInput.addEventListener('keydown', (event) => {
-    if (event.key == 'Enter' && cityInput.value.trim() != ''
-    ) {
-        updateWetherInfo();
+    if (cityInput.value.trim() != '') {
+        updateWetherInfo(cityInput.value);
         cityInput.value = '';
         cityInput.blur();
     }
 
 });
 
-async function getFetchData(endPoint,city) {
-const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apikey}`
-const response = await fetch(apiUrl);
-return response.json()
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' && cityInput.value.trim() != ''
+    ) {
+        updateWetherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apikey}`
+    const response = await fetch(apiUrl);
+
+    return response.json()
 }
+
 async function updateWetherInfo(city) {
-const weatherData = await getFetchData('weather',city)
-console.log(weatherData);
+    const weatherData = await getFetchData('weather', city)
+    console.log(weatherData)
 }
+
